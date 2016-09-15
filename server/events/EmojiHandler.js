070cf93df4341f700abf088f037ac009dcaab2cc
PieterJanVdb <pj.vandenbussche@gmail.com>
2016-09-16 00:27:02 +0200

    show interactive message in chat

--- server/events/EmojiHandler.js
+++ server/events/EmojiHandler.js
@@ -1,6 +1,7 @@
 const Queue = require('bull');
 const download = require('download');
 const fs = require('fs-promise');
+const request = require('request-promise');
 
 const emojiQueue = Queue('process_emoji_event', 6379, '127.0.0.1');
 
@@ -14,11 +15,16 @@ EmojiHandler.init = () => {
   });
 
   emojiQueue.on('error', err => console.error('EmojiHandler: ', err));
+  emojiQueue.on('completed', job => job.remove());
+  emojiQueue.on('failed', (job, err) => {
+    console.error('emojiQueue failed: ', err);
+    job.remove();
+  });
 }
 
 EmojiHandler.processAdded = emoji => {
   return new Promise((resolve, reject) => {
-    if (!emoji.value || !emoji.name || !emoji.team) reject();
+    if (!emoji.value || !emoji.name || !emoji.team) reject('emoji object incomplete');
 
     const splitted = emoji.value.split('.');
     const extension = splitted[splitted.length - 1];
@@ -32,7 +38,53 @@ EmojiHandler.processAdded = emoji => {
 };
 
 EmojiHandler.processRemoved = emoji => {
+  return new Promise((resolve, reject) => {
+    if (!emoji.name || !emoji.team) reject('emoji object incomplete');
 
+    const message = {
+      "text": "*The following emoji has been removed:* _" + emoji.name + "_",
+      "attachments": [
+        {
+          "text": "Would you like me to add the emoji back?",
+          "fallback": "You are unable to choose an action",
+          "callback_id": "emoji_action",
+          "color": "warning",
+          "actions": [
+            {
+              "name": "yes",
+              "text": "Yes",
+              "type": "button",
+              "style": "primary",
+              "value": emoji.name
+            },
+            {
+              "name": "no",
+              "text": "No",
+              "type": "button",
+              "value": emoji.name
+            }
+          ]
+        }
+      ]
+    };
+
+    const payload = {
+      "token": emoji.team.access_token,
+      "channel": process.env.CHANNEL,
+      "text": message.text,
+      "attachments": JSON.stringify(message.attachments)
+    };
+
+    const options = {
+      method: 'POST',
+      uri: 'https://slack.com/api/chat.postMessage',
+      formData: payload
+    };
+
+    request(options)
+      .then(parsedBody => resolve())
+      .catch(err => reject(err));
+  });
 };
 
 module.exports = EmojiHandler;
